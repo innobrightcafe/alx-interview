1128690f7cdda6b6d5a972584b08be274b9532a1
inosoft <innobrightcafe@gmail.com>
2023-07-02 17:25:00 -0400

    the pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,27 @@
-"""
-Defines the pascal_triangle variable 
-"""
+#!/usr/bin/python3
+""" Module to generate Pascals triangle in an array """
+
+
 def pascal_triangle(n):
+    """
+    function to find pascals triangle
+
+        Args:
+            n: integer numbers of rows to generate
+
+        Return:
+            base: pascal_triangle in an array
+    """
     if n <= 0:
         return []
-    triangle = [[1]]
-    """
-    Iterating the list
-    """
+
+    base = [[1]]
+
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
-            coefficient = triangle[i-1][j-1] + triangle[i-1][j]
-            row.append(coefficient)
+            row.append(base[i - 1][j - 1] + base[i - 1][j])
         row.append(1)
-        triangle.append(row)
+        base.append(row)
 
-    return triangle
+    return base

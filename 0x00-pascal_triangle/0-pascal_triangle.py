382e7c0df1ab9c016c4a702d925a81a87aa8cdb5
inosoft <innobrightcafe@gmail.com>
2023-07-02 13:04:45 -0400

    the pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,6 +5,9 @@ def pascal_triangle(n):
     if n <= 0:
         return []
     triangle = [[1]]
+    """
+    Iterating the list
+    """
     for i in range(1, n):
         row = [1]
         for j in range(1, i):

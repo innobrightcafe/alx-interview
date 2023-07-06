d574fdef1a41f8c069fe8d5d23f465e07eecbd70
inosoft <innobrightcafe@gmail.com>
2023-07-06 16:25:11 -0400

    Locked boxes interview

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,17 +5,11 @@ def canUnlockAll(boxes):
     Set to track visited boxes
     """
     visited = set()
-    """
-    Start with the first box
-    """ 
     stack = [0]  
 
     while stack:
         box = stack.pop()
         visited.add(box)
-        """
-        check to open all boxes
-        """
         for key in boxes[box]:
             if key not in visited and key < n:
                 stack.append(key)

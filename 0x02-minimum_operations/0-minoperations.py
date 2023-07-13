3dcfef65c22e2e185b8549e982e2f61ac8098858
inosoft <innobrightcafe@gmail.com>
2023-07-13 08:55:09 -0400

    Minimum numbers of operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,17 +2,16 @@
 """
 function to test for the minimum number of operations
 """
+
+
 def minOperations(n):
     if n == 1:
         return 0
-    
     operations = 0
     divisor = 2
-    
     while n > 1:
         while n % divisor == 0:
             operations += divisor
             n //= divisor
         divisor += 1
-    
     return operations

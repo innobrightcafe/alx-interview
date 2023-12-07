8a17ddc2a5776f6d0e0477337f25b2514f292663
Innocent A. Omodiale (Inosoft) <59028479+innobrightcafe@users.noreply.github.com>
2023-12-07 03:59:19 -0500

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -13,10 +13,12 @@ def canUnlockAll(boxes):
         if status[box] == "T" or box == 0:
             for key in boxes[box]:
                 if 0 <= key < n and status[key] == "F":
+                    """
+                    Update the status of the keys directly
+                    """
+                    status[key] = "T"
                     for k in boxes[key]:
                         if 0 <= k < n:
                             status[k] = "T"
-                if 0 <= key < n:
-                    status[key] = "T"
 
     return "F" not in status

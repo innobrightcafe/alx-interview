bd55f92f5924b4fb83cbe72f85eb5316b3605771
innobrightcafe <innobrightcafe@gmail.com>
2023-12-13 02:18:53 -0500

    minimum opts

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,14 +4,23 @@ function to test for the minimum number of operations
 """
 
 def minOperations(n):
-    if n == 1:
+    """
+    Fuction that gets fewest # of operations needed to result in exactly n H characters
+    """
+    # all outputs should be at least 2 char: (min, Copy All => Paste)
+    if (n < 2):
         return 0
-    operations = 0
-    divisor = 2
-    while n > 1:
-        while n % divisor == 0:
-            operations += divisor
-            n //= divisor
-        divisor += 1
-    return operations
+    opts, root = 0, 2
+    while root <= n:
+        # if n evenly divides by root
+        if n % root == 0:
+            # total even-divisions by root = total operations
+            opts += root
+            # set n to the remainder
+            n = n / root
+            # reduce root to find remaining smaller vals that evenly-divide n
+            root -= 1
+        # increment root until it evenly-divides n
+        root += 1
+    return opts
  
